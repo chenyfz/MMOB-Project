8dc494b294d849817ee100b228ab26b7760d743c
Y.C. <x.chenyangfan@gmail.com>
2024-06-14 15:06:43 +0200

    add consent

---
+++ src/components/consent.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p>
+    Before continuing, please take time to read the following:
+  </p>
+
+  <p class="title">
+    What is the purpose of this study?:
+  </p>
+
+  <p>
+    The purpose of this study is to test different additional visual feedback methods for tilt degree, to aid in controlling the tilt mechanism with respect to mobile gaming. The study is performed in the context of a 2D platformer game similar to Doodle Jump, a vertical jumping platformer game where you are guiding the character across platforms
+  </p>
+
+  <p class="title">
+    What will a participant do in this study?:
+  </p>
+
+  <p>
+    First, you will be asked to answer the first part for the questionnaire, concerning questions about demographics and prior experiences. Next, you will play three game version in random order. During the game play of each version, you play time and death count will be collected, while after playing each version, you will be asked to answer the second part of the questionnaire with questions about the game and player experience. Finally, you will be asked to answer the final part of the questionnaire, focussing on the comparision of the three game versions.
+  </p>
+
+  <p class="title">
+    How long will this study take?:
+  </p>
+
+  <p>
+    The study (gameplay + questionnaire) is expected to take around 15-30 minutes.
+  </p>
+  <p class="title">
+    What are the possible risks or discomforts?:
+  </p>
+
+  <p>
+    Excessive tilting of your mobile phone could cause strain on the wrist. Please, be mindful about this. If you feel any discomfort, do not hesitate to take a break between game versions.
+  </p>
+  <p class="title">
+    What is the conduct for data confidentially?:
+  </p>
+
+  <p>
+    Your responses will be kept strictly confidential, and digital data will be securely stored. Any publications based on this research will not include your name or any other individual information by which you could be identified. All of this is will be in line with the University of Utrecht: https://www.uu.nl/en/research/research-data-management/guides/policies-codes-of-conduct-and-laws.
+  </p>
+  <p class="title">
+    What are the rights of a participant?:
+  </p>
+
+  <p>
+    Participation in this study is voluntary. There will not be any compensation or reward for your participation. You may choose not to participate or, if you agreed to participate, you can withdraw your participation at any time without being penalized. If you would like to withdraw after the experiment has taken place the data from your experiment and questionnaire will be deleted.
+  </p>
+  <p class="title">
+    Are there questions, comments or concerns?:
+  </p>
+
+  <p>
+    Contact your contact person or send an email to
+    <a href="mailto:j.punt@students.uu.nl">j.punt@students.uu.nl</a>
+  </p>
+
+  <p>
+    If, after reading this information letter, you decide to take part in the study, please press the "Give consent" button to continue. Otherwise, press the "Quit" button.
+  </p>
+</template>
+
+<style scoped lang="stylus">
+p
+  margin-top: 4px
+  font-size 15px
+
+.title
+  margin-top: 16px
+  font-weight bold
+  text-decoration: underline;
+</style>
--- src/pages/intro-page.vue
+++ src/pages/intro-page.vue
@@ -5,10 +5,11 @@ import {writeParticipantData} from '../api'
 import {ref} from 'vue'
 import {ParticipantData} from '../types/participant-data.ts'
 import {getDeviceMotionPermission} from './device-motion-permission'
+import Consent from '../components/consent.vue'
 
 const resp = ref<Partial<ParticipantData>>({})
 
-const onSubmitForm = async () => {
+const onGiveConsent = async () => {
   const action = () => stageStore.stage = Stage.GAME
   if (await getDeviceMotionPermission()) {
     action()
@@ -25,34 +26,30 @@ const onTestServer = async () => {
 </script>
 
 <template>
-  <p>{{ resp }}</p>
-  <h3>👋Hi, Thanks for join our study!</h3>
-  <p>First, we would need your consent for this study: this study is about ...</p>
-  <p>
-    For any questions, please contact
-    <a href="mailto:y.chen40@students.uu.nl">y.chen40@students.uu.nl</a>
-  </p>
-
-  <label for="name-input">How should we call you?</label>
-  <input id="name-input" type="text">
-
-  <br>
-  <br>
-
-  <input id="consent-checkbox" type="checkbox">
-  <label for="consent-checkbox">I agree to be part of this study.</label>
-
-  <br>
-  <br>
-
-  <button @click="onSubmitForm">continue</button>
-
-  <br>
-  <br>
-
-  <button @click="onTestServer">test server</button>
+  <div class="intro-page">
+    <consent />
+    <v-btn
+      variant="flat"
+      color="primary"
+      block
+      class="text-none mt-4 mb-2"
+      @click="onGiveConsent"
+    >
+      Give consent
+    </v-btn>
+    <v-btn
+      variant="flat"
+      color="grey-lighten-3"
+      block
+      class="text-none"
+      @click="onTestServer"
+    >
+      Quit
+    </v-btn>
+  </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="stylus">
+.intro-page
+  margin 24px 12px 32px
 </style>
